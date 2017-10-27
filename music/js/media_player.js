91d6ef0e8b0aa80b63031af50285036c60d8fdc2
Jesse Rowley <rowleyj@mcmaster.ca>
2017-10-27 15:44:11 -0400

    added music files
    added listen-html
    altered browse -html
    added search
    added sort

---
+++ music/js/media_player.js
@@ -0,0 +1,11 @@
+//find audio file
+
+// play audio on click
+function playAudio() {
+  var audio = document.getElementById('boi');
+    x.play();
+}
+//pause audio on click
+function pauseAudio() {
+    x.pause();
+}
